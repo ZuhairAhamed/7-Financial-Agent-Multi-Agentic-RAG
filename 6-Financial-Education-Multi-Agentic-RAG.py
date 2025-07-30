18a1fc0aa1ba20ee6cc1d84a7d4a53905a03943c
Zuhair Ahamed <zuhairahamed4@gmail.com>
2025-07-30 22:02:46 +0300

    Update 6-Financial-Education-Multi-Agentic-RAG.py

--- 6-Financial-Education-Multi-Agentic-RAG.py
+++ 6-Financial-Education-Multi-Agentic-RAG.py
@@ -22,7 +22,7 @@ from langchain import hub
 from langgraph.graph.message import add_messages
 from langgraph.graph import END, StateGraph, START
 
-load_dotenv()
+#load_dotenv()
 
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
